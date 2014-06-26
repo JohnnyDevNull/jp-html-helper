76b9817a15395bc8279ed9b88aabb3398cdc1efe
JohnnyDevNull <JohnnyDevNull@users.noreply.github.com>
2014-06-27 00:49:26 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <html>
 	<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap-theme.min.css" />
